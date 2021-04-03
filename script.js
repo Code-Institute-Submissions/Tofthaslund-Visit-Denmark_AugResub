5cc19b08d3909580e7d1746b3c811cc06b593e15
Lasse Haslund <tofthaslund@gmail.com>
2021-04-03 19:28:07 +0100

    Google Maps with javascript and  one marker added to site

--- script.js
+++ script.js
@@ -5,5 +5,19 @@
 /* ? access_key = ecc8a2d2f68fa6411af0e18ae84df2aa;
 */
 
-// API key for Google maps
-gMap = AIzaSyA2YCmtB2HNATiYbczpoO6-FhWEYal5pNM
+// Function for Google Maps
+
+function initMap() {
+    // The location of Tivoli Garden
+    const tivoli = {lat: 55.67381073190113, lng: 12.568139473433078};
+    // The Map centered on Tivoli
+    const map = new google.maps.Map(document.getElementById("map"),{
+        zoom: 7,
+        center: tivoli,
+    });
+    // The marker, positioned at Tivoli
+    const marker = new google.maps.Marker({
+        position: tivoli,
+        map: map,
+    });
+}
