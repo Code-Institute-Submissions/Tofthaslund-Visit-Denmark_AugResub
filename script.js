5f2e4e4a9a17b671405a61b8cdacdba0bcff6e48
Lasse Haslund <tofthaslund@gmail.com>
2021-04-03 18:21:32 +0100

    HTML layout of Google Maps

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-// //API key for currency exhange 
+ //API key for currency exhange 
 
 // http://data.fixer.io/api/latest
 
 /* ? access_key = ecc8a2d2f68fa6411af0e18ae84df2aa;
 */
 
+// API key for Google maps
+gMap = AIzaSyA2YCmtB2HNATiYbczpoO6-FhWEYal5pNM
