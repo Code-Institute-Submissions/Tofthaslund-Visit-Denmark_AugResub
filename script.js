b3af9e71a626ddf026f3e9a908a3d98e141bb58d
Lasse Haslund <tofthaslund@gmail.com>
2021-04-04 14:53:43 +0100

    added more locations lat and lng to be added to google maps

--- script.js
+++ script.js
@@ -7,17 +7,27 @@
 
 // Function for Google Maps
 
-function initMap() {
-    // The location of Tivoli Garden
-    const tivoli = {lat: 55.67381073190113, lng: 12.568139473433078};
-    // The Map centered on Tivoli
-    const map = new google.maps.Map(document.getElementById("map"),{
-        zoom: 7,
-        center: tivoli,
-    });
-    // The marker, positioned at Tivoli
-    const marker = new google.maps.Marker({
-        position: tivoli,
-        map: map,
-    });
-}
+// function initMap() {
+//     {
+//     // The location of Tivoli Garden
+//     const tivoli = {lat: 55.67381073190113, lng: 12.568139473433078};
+//     // The Map centered on Tivoli
+//     const map = new google.maps.Map(document.getElementById("map"),{
+//         zoom: 7,
+//         center: tivoli,
+//     });
+//     // The marker, positioned at Tivoli
+//     const marker = new google.maps.Marker({
+//         position: tivoli,
+//         map: map,
+//     });
+// }
+// }
+
+var locations = [
+    ['Tivoli Garden', 55.67381073190113, 12.568139473433078, 4],
+    ['LegoLand', 55.73617509671368, 9.126426344705168, 3],
+    ['Bakken', 55.77533533280311, 12.577739212056098, 2],
+    ['The Old City', 56.158914026402215, 10.192158113918602, 1]
+];
+
