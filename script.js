83a6d9e2dfad8352dfc22087faa7bd0752b34342
Lasse Haslund <tofthaslund@gmail.com>
2021-04-06 16:22:03 +0100

    added click event to every marker

--- script.js
+++ script.js
@@ -8,36 +8,36 @@
 // Function for Google Maps
 
 function initMap() {
-    const myLatLng = { lat: 55.60012186704416, lng: 9.740083483716866 };
-    const tivoli = { lat: 55.67384138148071, lng: 12.568372404399566 };
-    const cphzoo = { lat: 55.672536335486996, lng: 12.521637048581432 };
-    const legoland = { lat: 55.742854035512835, lng: 9.132106277507289 };
-    const aahby = { lat: 56.1588064799222, lng:10.192522894633301 };
     const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 7,
-      center: myLatLng,
+        zoom: 7,
+        center: {lat: 55.60012186704416, lng: 9.740083483716866}
     });
-    new google.maps.Marker({
-      position: tivoli,
-      map,
-      title: "Tivoli Garden",
+
+const tourist = [
+    [{ lat: 55.67384138148071, lng: 12.568372404399566 }, "Tivoli Garden" ],
+    [{ lat: 55.672536335486996, lng: 12.521637048581432 }, "Copenhagn Zoo"],
+    [{ lat: 55.742854035512835, lng: 9.132106277507289 }, "LegoLand"],
+    [{ lat: 56.1588064799222, lng:10.192522894633301 }, "The Old City"]
+];
+
+const infoWindow = new google.maps.InfoWindow();
+
+//create markers.
+tourist.forEach(([position, title], i) => {
+    const marker = new google.maps.Marker({
+        position,
+        map,
+        title: `${i + 1}. ${title}`,
+        label: `${i + 1}`,
+        optimized: false,
     });
-    new google.maps.Marker({
-        position: cphzoo,
-        map,
-        title: "Tivoli Garden",
-      });
-      new google.maps.Marker({
-        position: legoland,
-        map,
-        title: "Tivoli Garden",
-      });
-      new google.maps.Marker({
-        position: aahby,
-        map,
-        title: "Tivoli Garden",
-      });
-  }
-  
-  
+
+marker.addListener("click", () => {
+    infoWindow.close();
+    infoWindow.setContent(marker.getTitle());
+    infoWindow.open(marker.getMap(), marker);
+    });
+});
+}
+
 
