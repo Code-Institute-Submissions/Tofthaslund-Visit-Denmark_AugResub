1eec435f212176eaaca099dcef432a450a26d38b
Lasse Haslund <tofthaslund@gmail.com>
2021-04-06 15:02:39 +0100

    added Den gamle By in Aarhus on google map

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ function initMap() {
     const tivoli = { lat: 55.67384138148071, lng: 12.568372404399566 };
     const cphzoo = { lat: 55.672536335486996, lng: 12.521637048581432 };
     const legoland = { lat: 55.742854035512835, lng: 9.132106277507289 };
+    const aahby = { lat: 56.1588064799222, lng:10.192522894633301 };
     const map = new google.maps.Map(document.getElementById("map"), {
       zoom: 7,
       center: myLatLng,
@@ -31,6 +32,12 @@ function initMap() {
         map,
         title: "Tivoli Garden",
       });
+      new google.maps.Marker({
+        position: aahby,
+        map,
+        title: "Tivoli Garden",
+      });
   }
+  
   
 
