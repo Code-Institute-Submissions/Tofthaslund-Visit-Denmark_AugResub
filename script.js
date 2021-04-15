8d0280adee9b98eb74e5e8c4db371cb5b6e69acc
Lasse Haslund <tofthaslund@gmail.com>
2021-04-15 11:59:39 +0100

    added javascript for dropdown content

--- script.js
+++ script.js
@@ -66,3 +66,23 @@ function calculate() {
 }
 
 calculate();
+
+
+
+// javascript for collapsible addmission fees
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+    // add eventlister with click funtion
+    coll[i].addEventListener("click", function(){
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.display === "block") {
+            content.style.display = "none";
+        } else{
+            content.style.display = "block";
+        }
+    });
+}
