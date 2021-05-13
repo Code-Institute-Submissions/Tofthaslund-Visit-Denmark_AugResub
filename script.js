c537d1ca2d5486d9c214e803c060ccdfedbe67a5
Lasse Haslund <tofthaslund@gmail.com>
2021-05-13 12:24:42 +0100

    added function for email form in JS

--- script.js
+++ script.js
@@ -86,3 +86,27 @@ for (i = 0; i < coll.length; i++) {
         }
     });
 }
+
+
+// email contact page
+
+const btn = document.getElementById('button');
+
+document.getElementById('form')
+.addEventListener('submit', function(event){
+    event.preventDefault();
+
+    btn.value = 'Sending...';
+
+    const serviceID = 'default_service';
+    const templateID = 'lasse ';
+
+    emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+        btn.value = 'Send Email';
+        alert('Great, we will be in touch')
+    }, (err) => {
+        btn.value = 'Send Email';
+        alert(JSON.stringify(err));
+    });
+});
